7e1a541c3afd96ab4ec1f2932279837627418805
DANIEL_ALEJANDRO_GARCIA_HOLGUIN <a.21308051280481@cbtis128.edu.mx>
2024-06-04 14:05:34 +0000

    act

---
+++ lib/view/widgets/button.global.dart
@@ -0,0 +1,38 @@
+import 'package:garcia0l481/utils/global.colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ButtonGlobal extends StatelessWidget {
+  const ButtonGlobal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // ignore: avoid_print
+        print("Aerolineas Garcia");
+      },
+      child: Container(
+        alignment: Alignment.center,
+        height: 55,
+        decoration: BoxDecoration(
+          color: GlobalColors.mainColor,
+          borderRadius: BorderRadius.circular(6),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 10,
+            ), // BoxShadow
+          ],
+        ), // BoxDecoration
+        child: Text(
+          "Sign In",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+          ), // TextStyle
+        ), // Text
+      ),
+    );
+  }
+} // Container
